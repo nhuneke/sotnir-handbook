f04949aab85eb9c58c009e3a603de452e6a74978
nh6g15 <n.huneke@soton.ac.uk>
2021-07-27 11:58:09 +0200

    Update conf.py to include autosectionlabel extension

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,11 @@ release = '0.1.0'
 # ones.
 extensions = [
 	'sphinx_rtd_theme',
+	'sphinx.ext.autosectionlabel',
 ]
+
+# Make sure the target is unique
+autosectionlabel_prefix_document = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
